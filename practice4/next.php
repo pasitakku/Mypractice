bf1ebe6e9aaaf2e6348f42e299d2937cd4401828
Bosita Junjaided <pasita@kkumail.com>
2020-01-05 16:56:29 +0700

    Update next.php

--- practice4/next.php
+++ practice4/next.php
@@ -4,17 +4,14 @@
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 
-	if(!empty($_POST['username'])&& !empty($_POST['password'])){
-		$username= "bobby";
-		$password= "1234";
+	if($_POST['username'] == 'bobby' && $_POST['password'] == '1234'){
 		echo "success";
 	}else{
 		echo "incorrect";
 	}
 		
 ?>
-	
-	
+
 
 	
 	
