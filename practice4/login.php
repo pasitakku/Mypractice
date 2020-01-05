1d495d30822f56158f17a2ef960cd98dca1b2b04
Bosita Junjaided <pasita@kkumail.com>
2020-01-05 17:44:28 +0700

    อัพเดตตรวจสอบhash

--- practice4/login.php
+++ practice4/login.php
@@ -8,7 +8,7 @@
 <form action="next.php" method="POST">
 
 	Username : <input type="text" required name="username" placeholder="Username">
-	password : <input type="text" required name="password"placeholder="Password">
+	password : <input type="text" required name="password" placeholder="Password">
 	<input type="submit" value="sign in">
 </form>
 </body>
--- practice4/next.php
+++ practice4/next.php
@@ -4,14 +4,22 @@
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 
+/**
 	if($_POST['username'] == 'bobby' && $_POST['password'] == '1234'){
 		echo "success";
 	}else{
 		echo "incorrect";
 	}
-		
-?>
+		*/
+	 $hash = "$2y$12$5e11ba832810212345678uTlfrphgX/7TSeWLilQcHK2gRzziiy7.";
+
+if (password_verify($password, $hash)){
+   echo "success"; // Success!
+}
+else {
+   echo "Incorrect"; // Invalid credentials
+}
 
 
-	
+	?>
 	
